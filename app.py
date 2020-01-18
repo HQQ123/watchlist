90db934b6d0966e7f7b45d2f65b1d904055b82b8
hanqingqing <949912169@qq.com>
2020-01-19 00:17:02 +0800

    add index page

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for, escape
 
 app = Flask(__name__)
 
+name = 'hanqingqing'
+movies = [
+    {'title': 'My macos', 'year': '2018'},
+    {'title': 'My windows', 'year': '2013'},
+    {'title': 'My ubuntu', 'year': '2018'},
+]
+
 @app.route('/home')
 @app.route('/index')
-@app.route('/')
+@app.route('/hello')
 def hello():
     return '<h1>Welcome to My Watchlist</h1> <img src="http://helloflask.com/totoro.gif">'
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % escape(name)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/test')
 def test_url_for():
